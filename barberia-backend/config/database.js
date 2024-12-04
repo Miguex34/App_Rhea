a26c6be07cd41f94645a4ecc54a6872baeb277bf
Mig.G <mig.garate@duocuc.cl>
2024-12-04 00:38:17 -0300

    Nueva estructura

--- barberia-backend/config/database.js
+++ barberia-backend/config/database.js
@@ -6,9 +6,13 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql',
-    timezone: '-03:00',
+    dialectOptions: {
+      connectTimeout: 60000, // Extiende el tiempo de espera
+    },
   }
 );
+
 module.exports = sequelize;
