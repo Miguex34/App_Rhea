ae453aaa4d4264c435190cf6a3542c5aa01f89a4
Mig.G <mig.garate@duocuc.cl>
2024-12-04 16:52:58 -0300

    Se arreglan las imagenes

--- barberia-backend/middleware/upload.js
+++ barberia-backend/middleware/upload.js
@@ -4,7 +4,7 @@ const path = require('path');
 // Configuración de almacenamiento para multer
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/'); // Carpeta donde se guardarán las imágenes
+    cb(null, path.join(__dirname, '../uploads')); // Carpeta donde se guardarán las imágenes
   },
   filename: (req, file, cb) => {
     // Nombre único para cada archivo basado en la fecha actual
